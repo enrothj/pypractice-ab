d01a26a0dca37d3b67ee05d81f75bddfed83c23b
enrothj <jani.enroth@yahoo.com>
2023-11-17 19:01:02 +0200

    Implemented character stats

--- game/character.py
+++ game/character.py
@@ -1,5 +1,13 @@
 class Character:
     def __init__(self, name) -> None:
         self.name = name
-        self.hp = 10
-        
+        self.stats = {
+            "hp": 10,
+            "mp": 10,
+            "sp": 10,
+        }
+
+    def get_stat(self, name: str):
+        return self.stats[name]
+    
+    
