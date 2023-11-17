209a7fb28543dd4f08bc999dc86930a74bcb5b68
enrothj <jani.enroth@yahoo.com>
2023-11-17 19:55:46 +0200

    Basic character initialization

--- game/battle.py
+++ game/battle.py
@@ -1,4 +1,5 @@
 from game_board import GameBoard
+from entities import Character
 
 class Battle:
     """This class represents a single battle.
@@ -6,5 +7,9 @@ class Battle:
     Eventually, it will have parameters on what kind
     of a battle to create."""
 
+    def characters():
+        return [Character("Sam"), Character("Tom")]
+
     def __init__(self):
         self.board = GameBoard(5, 5)
+        self.board.init_characters(self.characters)
