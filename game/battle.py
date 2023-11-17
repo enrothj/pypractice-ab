0f97b8436bb6032065cf9539fe3a05aeaae45eed
enrothj <jani.enroth@yahoo.com>
2023-11-17 20:51:55 +0200

    Fixed game board init functions

--- game/battle.py
+++ game/battle.py
@@ -1,5 +1,5 @@
 from game_board import GameBoard
-from entities import Character
+from entities.character import Character
 
 class Battle:
     """This class represents a single battle.
@@ -7,20 +7,22 @@ class Battle:
     Eventually, it will have parameters on what kind
     of a battle to create."""
 
-    def characters():
+    def characters(self):
         return [Character("Sam"), Character("Tom")]
     
-    def terrain():
-        return [0 for _ in range(25)]
+    def terrain(self):
+        return [_ % 3 for _ in range(25)]
     
-    def obstacles():
+    def obstacles(self):
         obst = [0 for _ in range(25)]
         obst[1] = 0
         return obst
 
     def __init__(self):
         self.board = GameBoard(5, 5)
-        self.board.init_terrain(self.terrain)
-        self.board.init_obstacles(self.obstacles)
-        self.board.init_characters(self.characters)
+        self.board.init_terrain(self.terrain())
+        self.board.init_obstacles(self.obstacles())
+        self.board.init_characters(self.characters())
 
+    def get_board(self):
+        return self.board
