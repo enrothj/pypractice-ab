81c3aa2651c737aff6d6cf05470203439bbca56f
enrothj <jani.enroth@yahoo.com>
2023-11-17 20:07:15 +0200

    Added basic terrain generation

--- game/battle.py
+++ game/battle.py
@@ -9,7 +9,18 @@ class Battle:
 
     def characters():
         return [Character("Sam"), Character("Tom")]
+    
+    def terrain():
+        return [0 for _ in range(25)]
+    
+    def obstacles():
+        obst = [0 for _ in range(25)]
+        obst[1] = 0
+        return obst
 
     def __init__(self):
         self.board = GameBoard(5, 5)
+        self.board.init_terrain(self.terrain)
+        self.board.init_obstacles(self.obstacles)
         self.board.init_characters(self.characters)
+
